7624d641487cdead34c6f15a5c1b5cf7cd779ca3
Paulo Oliveira <57108685+OliveiraPauloC@users.noreply.github.com>
2021-03-05 17:34:01 -0300

    Exercício 13 concluido

--- semana4/lista-de-exercicios/exercicios.js
+++ semana4/lista-de-exercicios/exercicios.js
@@ -177,6 +177,14 @@ function filmeFavorito() {
 
 function imprimeChamada() {
    // implemente sua lógica aqui
+   const filme = {
+      nome: 'O Diabo Veste Prada',
+      ano: 2006,
+      diretor: 'David Frankel',
+      atores: ['Meryl Streep', 'Anne Hathaway', 'Emily Blunt', 'Stanley Tucci']
+   }
+   return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`
+
 }
 
 // Exercício 14
