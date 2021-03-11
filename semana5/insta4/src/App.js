f1140ca8d58018644f05c551993e4a7dfde0c66a
Paulo Oliveira <57108685+OliveiraPauloC@users.noreply.github.com>
2021-03-11 17:10:57 -0300

    Atualização da atividade

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -4,29 +4,43 @@ import Post from './components/Post/Post';
 
 import profileIcon from './img/profile.png'
 import labenuIcon from './img/labenu.png'
-import astrodevIcon from './img/astrodev.png'
+import astrobotIcon from './img/astrobot.png'
 
 class App extends React.Component {
+  state = {
+    post: [
+      {
+        nomeUsuario: 'devpaulo',
+        fotoUsuario: profileIcon,
+        fotoPost: 'https://picsum.photos/200/150?grayscale'
+      },
+      {
+        nomeUsuario: 'labenu',
+        fotoUsuario: labenuIcon,
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: 'astrobot',
+        fotoUsuario: astrobotIcon,
+        fotoPost: 'https://picsum.photos/200/150?blur=2'
+      }
+    ]
+  }
+
   render() {
+    const listaPost = this.state.post.map((post) => {
+      return (
+        <Post key={post.nomeUsuario}
+          nomeUsuario={ post.nomeUsuario }
+          fotoUsuario={ post.fotoUsuario }
+          fotoPost={ post.fotoPost }
+        />
+      )
+    })
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'devpaulo'}
-          fotoUsuario={ profileIcon }
-          fotoPost={'https://picsum.photos/200/150?grayscale'}
-        />
-
-        <Post
-          nomeUsuario={'labenu'}
-          fotoUsuario={ labenuIcon }
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-
-        <Post
-          nomeUsuario={'astrodev'}
-          fotoUsuario={ astrodevIcon }
-          fotoPost={'https://picsum.photos/200/150?blur=2'}
-        />
+        {listaPost}
       </div>
     );
   }
