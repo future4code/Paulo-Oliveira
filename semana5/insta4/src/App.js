1f613901db1db430c2f1761c52128bab61439b2a
Paulo Oliveira <57108685+OliveiraPauloC@users.noreply.github.com>
2021-03-11 19:34:18 -0300

    Atualização da atividade de React

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -6,6 +6,17 @@ import profileIcon from './img/profile.png'
 import labenuIcon from './img/labenu.png'
 import astrobotIcon from './img/astrobot.png'
 
+import styled from 'styled-components'
+
+const InputPost = styled.input`
+  margin: 5px;
+`
+
+const ButtonAdicionar = styled.button`
+  background-color: transparent;
+  margin: 10px;
+`
+
 class App extends React.Component {
   state = {
     post: [
@@ -24,7 +35,35 @@ class App extends React.Component {
         fotoUsuario: astrobotIcon,
         fotoPost: 'https://picsum.photos/200/150?blur=2'
       }
-    ]
+    ],
+
+    valorInputNome: '',
+    valorInputUsuario: '',
+    valorInputPost: ''
+  }
+
+  adicionarPost = () => {
+    const novoPost = {
+      nomeUsuario: this.state.valorInputNome,
+      fotoUsuario: this.state.valorInputUsuario,
+      fotoPost: this.state.valorInputPost
+    }
+
+    const novosPosts = [...this.state.post, novoPost]
+
+    this.setState({ post: novosPosts })
+  }
+
+  onChangeInputNome = (event) => {
+    this.setState({ valorInputNome: event.target.value })
+  }
+
+  onChangeInputUsuario = (event) => {
+    this.setState({ valorInputUsuario: event.target.value })
+  }
+
+  onChangeInputPost = (event) => {
+    this.setState({ valorInputPost: event.target.value })
   }
 
   render() {
@@ -40,6 +79,26 @@ class App extends React.Component {
 
     return (
       <div className={'app-container'}>
+        <InputPost 
+          value={this.state.valorInputNome}
+          onChange={this.onChangeInputNome}
+          placeholder={'Nome de Usuário'}
+        />
+
+        <InputPost
+          value={this.valorInputUsuario}
+          onChange={this.valorInputUsuario}
+          placeholder={'Foto de Perfil'}
+        />
+
+        <InputPost
+          value={this.valorInputPost}
+          onChange={this.valorInputPost}
+          placeholder={'Foto que deseja postar'}
+        />
+
+        <ButtonAdicionar onClick={this.adicionarPost}>Postar</ButtonAdicionar>
+
         {listaPost}
       </div>
     );
